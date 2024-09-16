0e7179c4f3332cd5168b504ca6b960accb03b6c2
Sara Poursanati <sara_poursanati@outlook.com>
2024-09-16 10:40:11 +0330

    Common changed applied.

--- src/main/java/ir/niopdc/policy/service/PolicyService.java
+++ src/main/java/ir/niopdc/policy/service/PolicyService.java
@@ -2,9 +2,9 @@ package ir.niopdc.policy.service;
 
 import com.google.protobuf.ByteString;
 import io.grpc.stub.StreamObserver;
-import ir.niopdc.commons.grpc.PolicyRequest;
-import ir.niopdc.commons.grpc.PolicyResponse;
-import ir.niopdc.commons.grpc.PolicyServiceGrpc;
+import ir.niopdc.commons.grpc.policy.PolicyRequest;
+import ir.niopdc.commons.grpc.policy.PolicyResponse;
+import ir.niopdc.commons.grpc.policy.PolicyServiceGrpc;
 import ir.niopdc.policy.dto.PolicyDto;
 import ir.niopdc.policy.facade.PolicyFacade;
 import net.devh.boot.grpc.server.service.GrpcService;
