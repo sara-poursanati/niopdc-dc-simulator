88b22241f4dd02bb80b48676effcb50c82822eeb
Sara Poursanati <sara_poursanati@outlook.com>
2024-10-22 14:23:00 +0330

    Scheduling enabled

--- src/main/java/ir/niopdc/policy/config/AppConfig.java
+++ src/main/java/ir/niopdc/policy/config/AppConfig.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan(basePackages = {"ir.niopdc.*"})
 @EnableAspectJAutoProxy
+@EnableScheduling
 public class AppConfig {
 
     @Bean
