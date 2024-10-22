c9510972f107b6146f7483a84b93f1133bb67248
mahdiye <tadayon.mahdiye@gmail.com>
2024-10-22 09:55:39 +0330

    Add Paginated Blacklist Records

--- src/main/java/ir/niopdc/policy/domain/blacklist/BlackListRepository.java
+++ src/main/java/ir/niopdc/policy/domain/blacklist/BlackListRepository.java
@@ -1,9 +1,13 @@
 package ir.niopdc.policy.domain.blacklist;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 @Repository
@@ -11,4 +15,7 @@ interface BlackListRepository extends ListCrudRepository<BlackList, String> {
 
     @Query("select b from BlackList b")
     Stream<BlackList> streamAll();
+
+    @Query("SELECT b FROM BlackList b WHERE b.insertionDateTime > :time ORDER BY b.insertionDateTime ASC")
+    Page<BlackList> findAllByInsertionDateTimeAfter(@Param("time") LocalDateTime time, Pageable pageable);
 }
