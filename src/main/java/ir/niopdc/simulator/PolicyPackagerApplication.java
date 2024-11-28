c01d9544cb03d5d7c9774aa549e0494687db36d5
Sara Poursanati <sara_poursanati@outlook.com>
2024-11-28 12:32:22 +0330

    Initial Commit

---
+++ src/main/java/ir/niopdc/simulator/PolicyPackagerApplication.java
@@ -0,0 +1,19 @@
+package ir.niopdc.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"ir.niopdc.*"})
+@EnableAspectJAutoProxy
+@EnableScheduling
+public class PolicyPackagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyPackagerApplication.class, args);
+	}
+
+}
