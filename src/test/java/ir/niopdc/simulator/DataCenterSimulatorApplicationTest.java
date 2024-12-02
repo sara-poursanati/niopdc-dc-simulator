27044490a9b1a53e4e09cceadaf518c766655b0e
Sara Poursanati <sara_poursanati@outlook.com>
2024-12-02 12:25:31 +0330

    Test application renamed.

--- src/test/java/ir/niopdc/simulator/DataCenterSimulatorApplicationTest.java
+++ src/test/java/ir/niopdc/simulator/DataCenterSimulatorApplicationTest.java
@@ -1,10 +1,10 @@
-package ir.niopdc.simulator.policy;
+package ir.niopdc.simulator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PolicyMetadataPackagerApplicationTests {
+class DataCenterSimulatorApplicationTest {
 
 	@Test
 	void contextLoads() {
