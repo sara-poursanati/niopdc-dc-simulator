48c6366aeb119e5bb4428c5bb8e7f4d2e4acd02f
Mohammad Mahdi <mohammadmahdispot@gmail.com>
2024-12-02 05:33:51 -0800

    gray history service class

---
+++ src/main/java/ir/niopdc/simulator/domain/grayhistory/GrayHistoryService.java
@@ -0,0 +1,9 @@
+package ir.niopdc.simulator.domain.grayhistory;
+
+import ir.niopdc.common.entity.base.BaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GrayHistoryService extends BaseService<GrayHistoryRepository, GrayHistory, String> {
+}
+
