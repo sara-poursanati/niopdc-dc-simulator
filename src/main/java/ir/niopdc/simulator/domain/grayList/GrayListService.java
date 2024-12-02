21a5ecdfc09a3c9c7c4d443ac9ceb52a4cf8eaa7
Mohammad Mahdi <mohammadmahdispot@gmail.com>
2024-12-01 23:56:43 -0800

    remove methode

---
+++ src/main/java/ir/niopdc/simulator/domain/grayList/GrayListService.java
@@ -0,0 +1,8 @@
+package ir.niopdc.simulator.domain.grayList;
+
+import ir.niopdc.simulator.base.BaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GrayListService extends BaseService<GrayListRepository, GrayList, String> {
+}
