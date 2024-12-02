013ba6423d6a9f6d9163a23f9ccd4054ebcccbf6
Mohammad Mahdi <mohammadmahdispot@gmail.com>
2024-12-01 23:59:20 -0800

    import methods to generate fake data

--- src/main/java/ir/niopdc/simulator/scheduler/GrayListScheduler.java
+++ src/main/java/ir/niopdc/simulator/scheduler/GrayListScheduler.java
@@ -1,23 +1,38 @@
 package ir.niopdc.simulator.scheduler;
 
-import ir.niopdc.simulator.gray.GrayList;
-import ir.niopdc.simulator.gray.GrayListService;
+import ir.niopdc.simulator.domain.grayList.GrayList;
+import ir.niopdc.simulator.domain.grayList.GrayListService;
+import ir.niopdc.simulator.domain.grayList.GrayListType;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.ZonedDateTime;
+import java.util.Random;
+import java.util.UUID;
 
 @Component
 public class GrayListScheduler {
 
     private final GrayListService grayListService;
+    private final Random random = new Random();
 
     public GrayListScheduler(GrayListService grayListService) {
         this.grayListService = grayListService;
     }
 
     @Scheduled(fixedRate = 100000)
-    public void generateGrayCard() {
-        GrayList grayCard = grayListService.generateGrayList();
-        System.out.println("Gray Card created: " + grayCard.getCard_id());
+    public void generateGrayList() {
+        GrayList grayList = new GrayList();
+        grayList.setCardId(UUID.randomUUID().toString());
+        grayList.setValideId(UUID.randomUUID().toString());
+        grayList.setReason(RandomStringUtils.randomAlphabetic(20));
+        grayList.setType(GrayListType.values()[random.nextInt(GrayListType.values().length)]);
+        grayList.setDateTime(ZonedDateTime.now());
+        grayList.setUserId(RandomStringUtils.randomNumeric(10));
+
+        grayListService.save(grayList);
+        System.out.println("Gray Card created: " + grayList.getCardId());
     }
 }
 
