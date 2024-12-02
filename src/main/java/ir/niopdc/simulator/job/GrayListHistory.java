d5dec1064ea0b225271b8dcc037fed60ed9c63b9
Mohammad Mahdi <mohammadmahdispot@gmail.com>
2024-12-02 05:38:25 -0800

    gray list-history class with "removeGrayHistory" method to remove from gray list table

---
+++ src/main/java/ir/niopdc/simulator/job/GrayListHistory.java
@@ -0,0 +1,43 @@
+package ir.niopdc.simulator.job;
+
+import ir.niopdc.simulator.domain.grayhistory.GrayHistory;
+import ir.niopdc.simulator.domain.grayhistory.GrayHistoryService;
+import ir.niopdc.simulator.domain.graylist.GrayList;
+import ir.niopdc.simulator.domain.graylist.GrayListService;
+import jakarta.persistence.PreRemove;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class GrayListHistory {
+
+    private final GrayHistoryService grayHistoryService;
+    private final GrayListService grayListService;
+
+    public GrayListHistory(GrayHistoryService grayHistoryService, GrayListService grayListService) {
+        this.grayHistoryService = grayHistoryService;
+        this.grayListService = grayListService;
+    }
+
+    public Optional<GrayList> removeFromGrayList(String cardId) {
+        Optional<GrayList> grayList = grayListService.getRepository().findById(cardId);
+        grayList.ifPresent(grayListService.getRepository()::delete);
+        return grayList;
+    }
+
+    @PreRemove
+    public void removeGrayHistory(GrayList grayList) {
+        GrayHistory grayHistory = new GrayHistory(
+                grayList.getCardId(),
+                grayList.getValideId(),
+                grayList.getReason(),
+                grayList.getType().toString(),
+                grayList.getUserId(),
+                new java.sql.Timestamp(System.currentTimeMillis()),
+                "D"
+        );
+
+        grayHistoryService.save(grayHistory);
+    }
+}
