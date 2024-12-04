12ab53be68c1f4fa10e32a1c19c09f0dc4fa3663
Mohammad Mahdi <mohammadmahdispot@gmail.com>
2024-12-04 02:52:41 -0800

    change "removeGrayHistory" method and add logic in for

--- src/main/java/ir/niopdc/simulator/job/GrayListHistory.java
+++ src/main/java/ir/niopdc/simulator/job/GrayListHistory.java
@@ -4,10 +4,12 @@ import ir.niopdc.simulator.domain.grayhistory.GrayHistory;
 import ir.niopdc.simulator.domain.grayhistory.GrayHistoryService;
 import ir.niopdc.simulator.domain.graylist.GrayList;
 import ir.niopdc.simulator.domain.graylist.GrayListService;
-import jakarta.persistence.PreRemove;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.sql.Timestamp;
+import java.util.List;
 
 @Component
 public class GrayListHistory {
@@ -15,29 +17,28 @@ public class GrayListHistory {
     private final GrayHistoryService grayHistoryService;
     private final GrayListService grayListService;
 
+
     public GrayListHistory(GrayHistoryService grayHistoryService, GrayListService grayListService) {
         this.grayHistoryService = grayHistoryService;
         this.grayListService = grayListService;
     }
 
-    public void removeFromGrayList(String cardId) {
-        Optional<GrayList> grayList = grayListService.getRepository().findById(cardId);
-        grayList.ifPresent(list -> grayListService.deleteById(cardId));
-    }
-
-
-    @PreRemove
-    public void removeGrayHistory(GrayList grayList) {
-        GrayHistory grayHistory = new GrayHistory(
-                grayList.getCardId(),
-                grayList.getValideId(),
-                grayList.getReason(),
-                grayList.getType().toString(),
-                grayList.getUserId(),
-                new java.sql.Timestamp(System.currentTimeMillis()),
-                "D"
-        );
-
-        grayHistoryService.save(grayHistory);
+    @Scheduled(cron = "${app.config.cron.gray-list}")
+    @Transactional
+    public void removeGrayHistory() {
+        List<GrayList> grayLists = grayListService.getRepository().findAll();
+        for (GrayList grayList : grayLists) {
+            GrayHistory grayHistory = new GrayHistory(
+                    grayList.getCardId(),
+                    grayList.getValideId(),
+                    grayList.getReason(),
+                    grayList.getType().toString(),
+                    grayList.getUserId(),
+                    new Timestamp(System.currentTimeMillis()),
+                    "D"
+            );
+            grayHistoryService.save(grayHistory);
+            grayListService.deleteById(grayList.getCardId());
+        }
     }
 }
