1e6e59518f93a361de74e6ffa382be170183c1d2
Mohammad Mahdi <mohammadmahdispot@gmail.com>
2024-12-04 05:11:02 -0800

    add method find for 10 record.

--- src/main/java/ir/niopdc/simulator/domain/graylist/GrayListRepository.java
+++ src/main/java/ir/niopdc/simulator/domain/graylist/GrayListRepository.java
@@ -3,6 +3,10 @@ package ir.niopdc.simulator.domain.graylist;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GrayListRepository extends ListCrudRepository<GrayList, String> {
+
+    List<GrayList> findTop10By();
 }
